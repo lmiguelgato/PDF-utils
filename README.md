af13783b83fcc92e1d8ff0f07ce9f40eb04c4cc4
Luis Gato Diaz <luisgatodiaz@microsoft.com>
2022-09-19 19:11:26 -0500

    Py2 to Py3

--- README.md
+++ README.md
@@ -1,8 +1,1 @@
-# PythonCombinePDF
-Combine PDFs local
-
-All files to combine must be with in the same folder as the script. 
-
-Run script in Python Idle or in command line 
-
-https://www.pythoncentral.io/execute-python-script-file-shell/
+Forked and enhanced, after the original: https://github.com/titse/PythonCombinePDF
--- combinePDF.py
+++ combinePDF.py
@@ -1,21 +1,16 @@
-# combinePDF.py
-# Author: Tiffany Tse
-# Date: Oct 09,2018
-# Updated: Oct 10, 2018
-# Description: Combining pdf locally
-# Dependencies: os, pyPDF2
+''' Forked and enhanced, after the original: https://github.com/titse/PythonCombinePDF '''
 
 import os
 from PyPDF2 import PdfFileMerger
 
 # Directory of the where script and py are located
 fileDir = os.path.dirname(os.path.realpath(__file__))
-print "This is fileDir: " + fileDir
+print(f"This is fileDir: {fileDir}")
 
 #pdf get all the pdfs to merge
 pdfToMerge = [pdfIndex for pdfIndex in os.listdir(fileDir) if pdfIndex.endswith(".pdf")]
-print "The files to merge are:"
-print pdfToMerge
+print("The files to merge are:")
+print(pdfToMerge)
 merger = PdfFileMerger()
 
 for pdf in pdfToMerge:
@@ -25,4 +20,4 @@ with open("result.pdf", "wb") as fout:
     merger.write(fout)
     merger.close()
 
-print "Done!"
+print("Done!")
